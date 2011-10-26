5abe2c776e50340b0f41d5516b9ff18fa5853ae9
Julia <juliafanny@gmail.com>
2011-10-26 21:47:49 +0200

    testing controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,30 @@
 class ProjectsController < ApplicationController
+
+	def new
+    @project = Project.new()
+  end
+
+	def create
+    @project = Project.new(params[:project])
+
+    respond_to do |format|
+      if @project.save
+        format.html { redirect_to @project, notice: 'User was successfully created.' }
+        format.json { render json: @project, status: :created, location: @project }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @project.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+	def show
+    @project = Project.find(params[:id])
+    #@task = Task.new( :project => @project )
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @project }
+    end
+  end
 end
--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe ProjectsController do
+	before do
+  	@project = Factory(:project)
+	end
+
+	it "should have a create action" do
+	post :create, :project => {:name => "foo"}
+  end
+
+  it "should have a show action" do
+    get :show, :id => @project
+    assigns(:project).should == @project
+  end
 
 end
